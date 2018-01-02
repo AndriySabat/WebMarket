7a714c94ffe4d41d82535e0e47bb0cde97f8868f
AndriySabat <andriy.sabat.96@gmail.com>
2018-01-02 23:23:49 +0200

    general implementation

--- WebMarket.WebAPI/Controllers/TestController.cs
+++ WebMarket.WebAPI/Controllers/TestController.cs
@@ -7,20 +7,20 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebMarket.WebAPI.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class TestController : Controller
     {
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public string Get()
         {
-            return new string[] { "value1", "value2" };
+            return "It`s working";
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "value" + id.ToString();
         }
 
         // POST api/values
