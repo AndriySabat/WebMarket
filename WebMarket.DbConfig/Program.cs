91c50fc35dfa0a19a0f6564bfd689e0d9dbff825
AndriySabat <andriy.sabat.96@gmail.com>
2018-01-12 14:42:21 +0200

    added temp project for DB setup

---
+++ WebMarket.DbConfig/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using WebMarket.EF;
+
+namespace WebMarket.DbConfig
+{
+    /// <summary>
+    /// Class Program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Gets or sets the configuration.
+        /// </summary>
+        /// <value>The configuration.</value>
+        public static IConfigurationRoot Configuration { get; set; }
+
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        public static void Main()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true);
+
+            Configuration = builder.Build();
+
+            ConnectionStringGetter.ConnectionString = Configuration.GetSection("ConnectionStrings:DefaultConnection").Value;
+        }
+    }
+}
