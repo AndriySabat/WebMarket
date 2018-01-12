99154e6900c4bc687217d0007d7b372796b1ee9a
AndriySabat <andriy.sabat.96@gmail.com>
2018-01-12 14:45:57 +0200

    added common project and interface for dependency injection

---
+++ WebMarket.Common/IDependencyConfigurator.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WebMarket.Common
+{
+    public interface IDependencyConfigurator
+    {
+        void Configure(IServiceCollection services);
+    }
+}
